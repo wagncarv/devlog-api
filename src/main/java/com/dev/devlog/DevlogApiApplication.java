34f571131726e721210dd77d1b41c5650a9d757b
Wagner Carvalho <wagncarv@gmail.com>
2021-10-18 23:27:50 -0300

    Initial commit

---
+++ src/main/java/com/dev/devlog/DevlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.devlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlogApiApplication.class, args);
+	}
+
+}

81944ea42efbf24d7bc2cfe5b4fd2d1ccf9a26c5
Wagner Carvalho <wagncarv@gmail.com>
2021-10-21 03:18:06 -0300

    jakarta jpa

--- src/main/java/com/dev/devlog/domain/model/Cliente.java
+++ src/main/java/com/dev/devlog/domain/model/Cliente.java
@@ -1,13 +1,29 @@
 package com.dev.devlog.domain.model;
 
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@Data
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String nome;
+	private String name;
 	private String email;
+	
+	@Column(name = "fone")
 	private String telefone;
 	
 

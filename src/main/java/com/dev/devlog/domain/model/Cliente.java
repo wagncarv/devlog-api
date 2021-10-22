f7f3c45b1cf8eb089711b183c4c37dd8967dbaba
Wagner Carvalho <wagncarv@gmail.com>
2021-10-21 22:05:48 -0300

    crud Cliente, speing data

--- src/main/java/com/dev/devlog/domain/model/Cliente.java
+++ src/main/java/com/dev/devlog/domain/model/Cliente.java
@@ -20,7 +20,7 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private String nome;
 	private String email;
 	
 	@Column(name = "fone")
